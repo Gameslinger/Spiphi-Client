f14d49286341b80844c616fa8ed1f5960b5f76a8
Bennett.DenBleyker <Bennett.DenBleyker@DRA2-209-13.win-domain.apaserver.org>
2017-09-29 14:26:16 -0600

    Refactor

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -5,86 +5,19 @@
  */
 package Main;
 
-import java.net.InetAddress;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 /**
  *
  * @author Bennett.DenBleyker
  */
-public class Main extends ACommunication {
-
-    public static Main p2p;
+public class Main {
+    
+    public static P2P p2p;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        p2p = new Main();
+        p2p = new P2P();
         Option.options();
     }
-
-    final ScheduledExecutorService execService = Executors.newSingleThreadScheduledExecutor();
-
-    Callable<Void> task = new Callable() {
-        @Override
-        public Class<Void> call() {
-            refresh();
-            return Void.TYPE;
-        }
-    };
-
-    public Main() {
-        try {
-            preStart();
-            task.call();
-        } catch (Exception ex) {
-
-        }
-    }
-
-    @Override
-    public void refresh() {
-        System.out.println("Hi");
-        execService.schedule(task, ((Integer) getOption("Refresh").getValue()), TimeUnit.MINUTES);
-    }
-
-    @Override
-    public void addFriend(InetAddress ip) {
-        
-    }
-
-    @Override
-    public void addFriend(String username) {
-        
-    }
-
-    @Override
-    public boolean createRequest() {
-        return true;
-    }
-
-    @Override
-    public boolean acceptRequest() {
-        return true;
-    }
-
-    @Override
-    public boolean denyRequest() {
-        return true;
-    }
-
-    @Override
-    public void start() {
-        
-    }
-
-    @Override
-    public void setup() {
-        
-    }
-
 }
---
+++ src/Main/P2P.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import java.net.InetAddress;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author Bennett.DenBleyker
+ */
+public class P2P extends ACommunication {
+
+    final ScheduledExecutorService execService = Executors.newSingleThreadScheduledExecutor();
+
+    Callable<Void> task = new Callable() {
+        @Override
+        public Class<Void> call() {
+            refresh();
+            return Void.TYPE;
+        }
+    };
+
+    public P2P() {
+        try {
+            preStart();
+            task.call();
+        } catch (Exception ex) {
+
+        }
+    }
+
+    @Override
+    public void refresh() {
+        System.out.println("Hi");
+        execService.schedule(task, ((Integer) getOption("Refresh").getValue()), TimeUnit.MINUTES);
+    }
+
+    @Override
+    public void addFriend(InetAddress ip) {
+        
+    }
+
+    @Override
+    public void addFriend(String username) {
+        
+    }
+
+    @Override
+    public boolean createRequest() {
+        return true;
+    }
+
+    @Override
+    public boolean acceptRequest() {
+        return true;
+    }
+
+    @Override
+    public boolean denyRequest() {
+        return true;
+    }
+
+    @Override
+    public void start() {
+        
+    }
+
+    @Override
+    public void setup() {
+        
+    }
+
+}
